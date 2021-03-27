e2aac42b2ecaa253d75290ab8a407ada2a99989f
prodiegus <diego.d.a.f.v.2000@gmail.com>
2021-03-27 15:58:27 -0300

    funcion eliminar correcta

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -7,10 +7,12 @@ class Main {
     adminBanco.Agregar(1,12760310, "Paolo Rivera");
     adminBanco.Agregar(0,18218955, "Francisco Espinoza");
     adminBanco.Agregar(1,177878345, "Javiera Rojas");
-    //for(int i=0; i<adminBanco.getNmroCuentas(); i++){
-      //System.out.println(adminBanco.getCuentas(i));
+    adminBanco.Eliminar(45547385);
+    System.out.println("------------------------------Borrado numero de cuenta 45547385---------------------------------");
+    for(int i=0; i<adminBanco.getNmroCuentas(); i++){
+      System.out.println(adminBanco.getCuenta(i));
 
-    //}
+    }
 
   }
 }
