f095a5d19cd5a06355bf880dc116041a4750f9d3
prodiegus <diego.d.a.f.v.2000@gmail.com>
2021-03-28 19:36:26 -0300

    hay un glich en los scaners

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws FileNotFoundException {
@@ -28,24 +28,28 @@ class Main {
             	break;
 
 			}
+			System.out.print("Oprima 1 para salir: ");
+
 		}
+		lectura.close();
   	}
   	public static void crearCuenta(Banco adminBanco){ 
-		    Scanner lectura = new Scanner(System.in);
-		    System.out.println("Usted a ingresado al menu de creacion de cuentas:");
-		    System.out.println("Ingrese los dados de la cuenta en el siguente orden:"
-    	                    +"\n1.-tipocuenta[0/1](0 = Cuenta ahorro y 1 = Cuenta vista)"
-		                      +"\n2.-Rut Usuario"
-		                      +"\n3.-Nombre Usuario(Nombre Apellido)\n");
-		    adminBanco.Agregar(lectura.nextInt(),lectura.nextInt(),(lectura.next()+" "+lectura.next()));
-		    System.out.println("\n");
+		Scanner lectura = new Scanner(System.in);
+		System.out.println("Usted a ingresado al menu de creacion de cuentas:");
+	    System.out.println("Ingrese los dados de la cuenta en el siguente orden:"
+                        +"\n1.-tipocuenta[0/1](0 = Cuenta ahorro y 1 = Cuenta vista)"	                      +"\n2.-Rut Usuario"
+		                +"\n3.-Nombre Usuario(Nombre Apellido)\n");
+		adminBanco.Agregar(lectura.nextInt(),lectura.nextInt(),(lectura.next()+" "+lectura.next()));
+		System.out.println("\n");
+		lectura.close();
 
     }
-	  public static void eliminarCuenta(Banco adminBanco){
-		    Scanner lectura = new Scanner(System.in);
-		    System.out.println("Usted a ingresado a la opcion de eliminar cuenta: \n\n");
-        	System.out.print("Indique el numero de cuenta que desea eliminar:");
-		    adminBanco.Eliminar(lectura.nextInt());
+	public static void eliminarCuenta(Banco adminBanco){
+		Scanner lectura = new Scanner(System.in);
+		System.out.println("Usted a ingresado a la opcion de eliminar cuenta: \n\n");
+    	System.out.print("Indique el numero de cuenta que desea eliminar: ");
+	    adminBanco.Eliminar(lectura.nextInt());
+		lectura.close();
 
 	}
 
